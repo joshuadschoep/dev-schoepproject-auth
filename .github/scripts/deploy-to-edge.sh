1d64ea92f792f491114981ae12fe66bb13de3e21
Joshua D Schoep <joshuaschoep@gmail.com>
2024-02-07 15:20:29 -0700

    retry 4

--- .github/scripts/deploy-to-edge.sh
+++ .github/scripts/deploy-to-edge.sh
@@ -1,14 +1,17 @@
 versionarn=`aws lambda publish-version --function-name=$AWS_FUNCTION_ARN --query="FunctionArn"`
 echo "Created new version $versionarn"
-aws cloudfront update-distribution --id=$AWS_CLOUDFRONT_DIST --distribution-config="{
-    \"LambdaFunctionAssociations\": {
-        \"Quantity\": 1,
-        \"Items\": [
+
+jq --arg $arn "$versionarn" '.LambdaFunctionAssociations = {
+        Quantity: 1,
+        Items: [
             {
-                \"LambdaFunctionARN\": $versionarn,
-                \"EventType\": \"viewer-request\",
-                \"IncludeBody\": false
+                LambdaFunctionARN: $arn,
+                EventType: "viewer-request",
+                IncludeBody: false
             }
         ]
-    }
-}"
+    }' > ./newconfig
+
+cat ./newconfig
+
+aws cloudfront update-distribution --id=$AWS_CLOUDFRONT_DIST --distribution-config=./newconfig
