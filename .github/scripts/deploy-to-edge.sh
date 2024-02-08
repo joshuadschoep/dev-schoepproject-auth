89318384f0ec20a96341144821760c7be765ff52
Joshua Schoep <joshuaschoep@gmail.com>
2024-02-08 11:50:17 -0700

    added etag check

--- .github/scripts/deploy-to-edge.sh
+++ .github/scripts/deploy-to-edge.sh
@@ -1,7 +1,10 @@
 versionarn=`aws lambda publish-version --function-name=$AWS_FUNCTION_ARN --query="FunctionArn"`
 echo "Created new version $versionarn"
 
-aws cloudfront get-distribution-config --id=$AWS_CLOUDFRONT_DIST --query="DistributionConfig" > oldconfig.json
+aws cloudfront get-distribution-config --id=$AWS_CLOUDFRONT_DIST > response.json
+
+etag=`jq ".ETag" response.json`
+jq ".DistributionConfig" response.json > oldconfig.json
 
 jq ".DefaultCacheBehavior.LambdaFunctionAssociations = {
         Quantity: 1,
@@ -16,4 +19,4 @@ jq ".DefaultCacheBehavior.LambdaFunctionAssociations = {
 
 cat newconfig.json;
 
-aws cloudfront update-distribution --id=$AWS_CLOUDFRONT_DIST --distribution-config file://newconfig.json
+aws cloudfront update-distribution --id=$AWS_CLOUDFRONT_DIST --if-match=$etag --distribution-config file://newconfig.json
