afafec3f6bd2bbdec5c4b468b22760468dc340a6
Joshua Schoep <joshuaschoep@gmail.com>
2024-02-08 11:55:24 -0700

    asdf

--- .github/scripts/deploy-to-edge.sh
+++ .github/scripts/deploy-to-edge.sh
@@ -1,10 +1,13 @@
-versionarn=`aws lambda publish-version --function-name=$AWS_FUNCTION_ARN --query="FunctionArn"`
+versionarn=`aws lambda publish-version --function-name $AWS_FUNCTION_ARN --query "FunctionArn"`
 echo "Created new version $versionarn"
 
-aws cloudfront get-distribution-config --id=$AWS_CLOUDFRONT_DIST > response.json
+aws cloudfront get-distribution-config --id $AWS_CLOUDFRONT_DIST > response.json
 
 etag=`jq ".ETag" response.json`
 jq ".DistributionConfig" response.json > oldconfig.json
+
+echo "Old configuration"
+cat oldconfig.json
 
 jq ".DefaultCacheBehavior.LambdaFunctionAssociations = {
         Quantity: 1,
@@ -17,6 +20,7 @@ jq ".DefaultCacheBehavior.LambdaFunctionAssociations = {
         ]
     }" oldconfig.json > newconfig.json
 
-cat newconfig.json;
+echo "New Configuration"
+cat newconfig.json
 
-aws cloudfront update-distribution --id=$AWS_CLOUDFRONT_DIST --if-match=$etag --distribution-config file://newconfig.json
+aws cloudfront update-distribution --id $AWS_CLOUDFRONT_DIST --if-match $etag --distribution-config file://newconfig.json
